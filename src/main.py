9dcb8aaac03afe07805fbf5f1d0b0a65553c9443
linaauddk <linauaddk@naver.com>
2024-05-28 06:19:36 +0000

    100

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ def main():
     flag = 0
     try:
         log(f"retrieving dataframe from database...")
-        lim = 10000
+        lim = 100
         log(f"with lim : {lim}")
         df = retrieve_df(lim, "tokenized", keys) # "lake", "tokenized", "warehouse"
         flag += 1 # 1
