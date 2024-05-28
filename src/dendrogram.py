4697e9e3bb210ab9f9f9b265ea5ebc19b84a6842
S0rrow <jae9hell@gmail.com>
2024-05-28 09:38:39 +0900

    dendrogram resulting images name fixed

--- src/dendrogram.py
+++ src/dendrogram.py
@@ -1,5 +1,6 @@
 from scipy.cluster.hierarchy import dendrogram
 import matplotlib.pyplot as plt
+from time import localtime, strftime
 
 def plot_dendrogram(clustering, labels):
     plt.figure(figsize=(10, 7))
@@ -7,5 +8,5 @@ def plot_dendrogram(clustering, labels):
     plt.xlabel('Distance')
     plt.ylabel('Document hash ID')
     plt.title('Hierarchical Clustering Dendrogram')
-    plt.savefig("./dendrogram.png")
+    plt.savefig(f"./dendrogram_{strftime("%H_%M_%S", localtime())}lim{len(labels)}.png")
     plt.show()
